65f621514514bc0251f4503becefc2cdf0204ce8
saebyeolKim <toquf2611@naver.com>
2023-01-31 18:03:54 +0900

    fix: 나머지가 1이 되는 수 찾기

--- src/main/java/LV1/FindRemainderOne.java
+++ src/main/java/LV1/FindRemainderOne.java
@@ -9,7 +9,7 @@ package LV1;
  */
 public class FindRemainderOne {
 
-    int fisrtCode(int n) {
+    int firstCode(int n) {
         int answer = 0;
         for (int i = 2; i < n; i++) {
             if(n % i == 1) {
